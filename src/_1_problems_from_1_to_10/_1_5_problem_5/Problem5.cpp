dc8b2b5c3dece93b69ee232032135f16bba74c16
m4md24 <m4md24@gmail.com>
2025-04-30 10:30:42 +0300

    # Solve Problem 5

---
+++ src/_1_problems_from_1_to_10/_1_5_problem_5/Problem5.cpp
@@ -0,0 +1,102 @@
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+short randomNumber(
+    const short FROM,
+    const short TO
+) { return rand() % (TO - FROM + 1) + FROM; }
+
+void fillMatrixWithRandomNumbers(
+    short matrixNumbers[3][3],
+    const short ROWS,
+    const short COLUMNS
+) {
+    for (short row = 0; row < ROWS; ++row)
+        for (short column = 0; column < COLUMNS; ++column)
+            matrixNumbers[row][column] = randomNumber(
+                -99,
+                999
+            );
+}
+
+void printMatrixNumbers(
+    const short MATRIX_NUMBERS[3][3],
+    const short ROWS,
+    const short COLUMNS,
+    const short NUMBER_WIDTH
+) {
+    for (short row = 0; row < ROWS; ++row) {
+        for (short column = 0; column < COLUMNS; ++column)
+            cout << setw(
+                NUMBER_WIDTH
+            ) << MATRIX_NUMBERS[row][column] << ' ';
+        cout << endl;
+    }
+}
+
+void calculateSumEachColumnMatrixNumbers(
+    const short MATRIX_NUMBERS[3][3],
+    short sumEachNumbers[3],
+    const short ROWS,
+    const short COLUMNS
+) {
+    for (int column = 0; column < COLUMNS; ++column) {
+        short columnSum = MATRIX_NUMBERS[0][column];
+        for (int row = 1; row < ROWS; ++row)
+            columnSum += MATRIX_NUMBERS[row][column];
+        sumEachNumbers[column] = columnSum;
+    }
+}
+
+void printSumEachColumns(
+    short sumEachColumns[3],
+    const short COLUMNS
+) {
+    for (int index = 0; index < COLUMNS; ++index)
+        cout << "Column " << index + 1 << " Sum = " << sumEachColumns[index] << endl;
+}
+
+int main() {
+    srand(
+        static_cast<unsigned>(
+            time(
+                nullptr
+            )
+        )
+    );
+
+    const short ROWS = 3,
+                COLUMNS = 3;
+    const short NUMBER_WIDTH = 3;
+    short matrixNumbers[ROWS][COLUMNS];
+    short sumEachColumns[ROWS];
+
+    fillMatrixWithRandomNumbers(
+        matrixNumbers,
+        ROWS,
+        COLUMNS
+    );
+
+    cout << "Matrix Numbers:" << endl;
+    printMatrixNumbers(
+        matrixNumbers,
+        ROWS,
+        COLUMNS,
+        NUMBER_WIDTH
+    );
+
+    cout << endl;
+
+    calculateSumEachColumnMatrixNumbers(
+        matrixNumbers,
+        sumEachColumns,
+        ROWS,
+        COLUMNS
+    );
+
+    printSumEachColumns(
+        sumEachColumns,
+        COLUMNS
+    );
+}
