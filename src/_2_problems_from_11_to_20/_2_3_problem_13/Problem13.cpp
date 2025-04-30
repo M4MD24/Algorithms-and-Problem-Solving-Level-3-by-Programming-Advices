a01c4431f0ded1a6d468f5c4454dd7a4eb7ab38a
m4md24 <m4md24@gmail.com>
2025-04-30 16:06:26 +0300

    # Solve Problem 13

---
+++ src/_2_problems_from_11_to_20/_2_3_problem_13/Problem13.cpp
@@ -0,0 +1,82 @@
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+void printMatrixNumbers(
+    const short MATRIX_NUMBERS[3][3],
+    const short ROWS,
+    const short COLUMNS,
+    const short NUMBER_WIDTH
+) {
+    for (short row = 0; row < ROWS; ++row) {
+        for (short column = 0; column < COLUMNS; ++column)
+            cout << setw(
+                NUMBER_WIDTH
+            ) << MATRIX_NUMBERS[row][column] << ' ';
+        cout << endl;
+    }
+}
+
+bool isIdentityMatrix(
+    const short MATRIX_NUMBERS[3][3],
+    const short ROWS,
+    const short COLUMNS
+) {
+    for (int row = 0; row < ROWS; ++row)
+        for (int column = 0; column < COLUMNS; ++column) {
+            if (row == column && MATRIX_NUMBERS[row][column] != 1)
+                return false;
+            if (row != column && MATRIX_NUMBERS[row][column] != 0)
+                return false;
+        }
+    return true;
+}
+
+int main() {
+    srand(
+        static_cast<unsigned>(
+            time(
+                nullptr
+            )
+        )
+    );
+
+    const short ROWS = 3,
+                COLUMNS = 3;
+    const short NUMBER_WIDTH = 2;
+    const short MATRIX_NUMBERS[ROWS][COLUMNS]{
+        {
+            1,
+            0,
+            0
+        },
+        {
+            0,
+            1,
+            0
+        },
+        {
+            0,
+            0,
+            1
+        }
+    };
+
+    cout << "Matrix Numbers:" << endl;
+    printMatrixNumbers(
+        MATRIX_NUMBERS,
+        ROWS,
+        COLUMNS,
+        NUMBER_WIDTH
+    );
+
+    cout << "\nIs" << (
+        isIdentityMatrix(
+            MATRIX_NUMBERS,
+            ROWS,
+            COLUMNS
+        )
+            ? ""
+            : "n't"
+    ) << " Identity Matrix";
+}
